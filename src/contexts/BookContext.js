1545b52801205a367730ab8e8416d197ae07f1e7
aminedammak <amine.dammak@softtodo.com>
2019-12-13 16:25:03 +0100

    Save data in localstorage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,14 +1,19 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
+import { parse } from '@babel/core';
 
 export const BookContext = createContext();
 
 export default function BookContextProvider(props) {
 
-    const [books, dispatch] = useReducer(bookReducer, [
-        { title: 'name of the wind', author: 'patrick rothfuss', id: 1 },
-        { title: 'the final empire', author: 'brandon sanderson', id: 2 },
-    ])
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const result = localStorage.getItem('books');
+        return result ? JSON.parse(result) : []
+    })
+
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books));
+    }, [books])
 
     return (
         <BookContext.Provider value={{
